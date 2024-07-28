cf7884e5c83fb6fbc00099fd25049044bdf0ff19
Zhihang CHENG <zhihang@Zhihangs-MacBook-Pro.local>
2024-07-27 22:58:36 -0700

    install necessary libraries and designed data model

---
+++ backend/models/Participant.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const participantSchema = new mongoose.Schema({
+  firstname: { type: String, required: true },
+  lastname: { type: String, required: true },
+  dateofbirth: { type: Date, required: true },
+  gender: { type: String, required: true },
+  race: { type: String, required: true },
+  livingLocation: { type: String, required: true },
+  workingLocation: { type: String, required: true },
+  housingStatus: { type: String, required: true },
+  educationLevel: { type: String, required: true },
+  healthInsurance: { type: Boolean, required: true },
+  dentalInsurance: { type: Boolean, required: true },
+  visionInsurance: { type: Boolean, required: true },
+  militaryOrVeteran: { type: Boolean, required: true },
+  orientationStatus: { type: String, required: true },
+  jobPlacement: {
+    jobTitle: { type: String, required: true },
+    startDate: { type: Date, required: true },
+    endDate: { type: Date }
+  }
+});
+
+const Participant = mongoose.model('Participant', participantSchema);
+
+module.exports = Participant;
